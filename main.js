3f44f92e2c8296f19e948f389774284f64f484fa
mazen <mazen.ali.rushdy@gmail.com>
2025-02-23 21:34:35 +0200

    m3

--- main.js
+++ main.js
@@ -2,5 +2,6 @@ document.querySelector("form input[type=submit]").onclick = function () {
     console.log('ma');
     console.log('fa');
     console.log("fares");
-    console.log("mahfouz")
+    console.log("mahfouz");
+    console.log('Mazen');
 }
